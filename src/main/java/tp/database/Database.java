127253b623255e5e72f73cebb5257fa6d4de9dec
ThomasT <trehou.thomas@outlook.fr>
2020-11-19 11:54:58 +0100

    Commit SELECT

--- src/main/java/tp/database/Database.java
+++ src/main/java/tp/database/Database.java
@@ -11,61 +11,42 @@ public class Database {
     //private final String user = "postgres";
     private final String user = "pornlkar";
     private final String password = "mbdpf5meee5IsCGJXyf80q3Gj9-L0lOz";
-
+    private static final String SELECT_ALL_QUERY_GLOBAL = "select * from global";
+    private static final String SELECT_ALL_QUERY_COUNTRY = "select * from countries where country_code =?";
+    private static final String SELECT_ALL_QUERY_CONFIRMED = "select sum(total_confirmed)/count(*) from countries";
+    private static final String SELECT_ALL_QUERY_AVG_DEATHS = "select sum(total_deaths)/count(*) from countries";
+    private static final String SELECT_ALL_QUERY_PERCENTS_DEATHS= "select (sum(total_deaths)/sum(total_confirmed))*100 from countries";
     //private final String password = "root";
 
     //
 
-    private final String CREATE_TABLE = "DROP TABLE IF EXISTS global; " +
-            "DROP TABLE IF EXISTS countries; " +
-            "CREATE TABLE global(" +
-            "    id INT PRIMARY KEY NOT NULL," +
-            "    new_confirmed bigint," +
-            "    total_confirmed bigint," +
-            "    new_deaths bigint, " +
-            "    total_deaths bigint," +
-            "    new_recovered bigint," +
-            "    total_recovered bigint," +
-            "    date_maj TIMESTAMP" +
-            "); " +
-            "CREATE TABLE countries(" +
-            "    country VARCHAR(200) PRIMARY KEY," +
-            "    country_code VARCHAR(6)," +
-            "    slug VARCHAR(200)," +
-            "    new_confirmed bigint," +
-            "    total_confirmed bigint," +
-            "    new_deaths bigint," +
-            "    total_deaths bigint," +
-            "    new_recovered bigint," +
-            "    total_recovered bigint," +
-            "    date_maj TIMESTAMP" +
-            ");";
+//    private final String CREATE_TABLE = "DROP TABLE IF EXISTS global; " +
+//            "DROP TABLE IF EXISTS countries; " +
+//            "CREATE TABLE global(" +
+//            "    id INT PRIMARY KEY NOT NULL," +
+//            "    new_confirmed bigint," +
+//            "    total_confirmed bigint," +
+//            "    new_deaths bigint, " +
+//            "    total_deaths bigint," +
+//            "    new_recovered bigint," +
+//            "    total_recovered bigint," +
+//            "    date_maj TIMESTAMP" +
+//            "); " +
+//            "CREATE TABLE countries(" +
+//            "    country VARCHAR(200) PRIMARY KEY," +
+//            "    country_code VARCHAR(6)," +
+//            "    slug VARCHAR(200)," +
+//            "    new_confirmed bigint," +
+//            "    total_confirmed bigint," +
+//            "    new_deaths bigint," +
+//            "    total_deaths bigint," +
+//            "    new_recovered bigint," +
+//            "    total_recovered bigint," +
+//            "    date_maj TIMESTAMP" +
+//            ");";
 
 
-    private final String INSERT_UPDATE_SQL = "INSERT INTO global (id, new_confirmed, total_confirmed, new_deaths, total_deaths, new_recovered, total_recovered, date_maj) " +
-            "VALUES " +
-            "(?, ?, ?, ?, ?, ?, ?, ?) " +
-            "ON CONFLICT (id) DO UPDATE " +
-            "SET new_confirmed = EXCLUDED.new_confirmed, " +
-            "total_confirmed = EXCLUDED.total_confirmed," +
-            "new_deaths = EXCLUDED.new_deaths, " +
-            "total_deaths = EXCLUDED.total_deaths," +
-            "new_recovered = EXCLUDED.new_recovered," +
-            "total_recovered = EXCLUDED.total_recovered," +
-            "date_maj = EXCLUDED.date_maj;";
 
-    private final String INSERT_UPDATE_SQL_COUNTRY = "INSERT INTO countries " +
-            "(country,country_code,slug,new_confirmed, total_confirmed, new_deaths, total_deaths, new_recovered, total_recovered, date_maj) " +
-            "VALUES " +
-            "(?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
-            "ON CONFLICT (country) DO UPDATE " +
-            "SET new_confirmed = EXCLUDED.new_confirmed, " +
-            "total_confirmed = EXCLUDED.total_confirmed," +
-            "new_deaths = EXCLUDED.new_deaths, " +
-            "total_deaths = EXCLUDED.total_deaths," +
-            "new_recovered = EXCLUDED.new_recovered," +
-            "total_recovered = EXCLUDED.total_recovered," +
-            "date_maj = EXCLUDED.date_maj;";
     public Connection connect() {
         Connection conn = null;
         try {
@@ -84,83 +65,163 @@ public class Database {
         return conn;
     }
 
-    public void createTable() throws SQLException {
+    public String getGlobal() {
+
 
         // Step 1: Establishing a Connection
-        try (Connection connection = connect();
-
+        try (Connection connection = DriverManager.getConnection(url, user, password);
              // Step 2:Create a statement using connection object
-             Statement statement = connection.createStatement();) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY_GLOBAL);) {
+             System.out.println(preparedStatement);
+             // Step 3: Execute the query or update query
+             ResultSet rs = preparedStatement.executeQuery();
 
-            // Step 3: Execute the query or update query
-            statement.execute(CREATE_TABLE);
-        } catch (SQLException e) {
-            // print SQL exception information
-            printSQLException(e);
+             // Step 4: Process the ResultSet object.
+            while(rs.next()) {
+            int id = rs.getInt("id");
+            int new_confirmed = rs.getInt("new_confirmed");
+            int total_confirmed = rs.getInt("total_confirmed");
+            int new_deaths = rs.getInt("new_deaths");
+            int total_deaths = rs.getInt("total_deaths");
+            int new_recovered = rs.getInt("new_recovered");
+            int total_recovered = rs.getInt("total_recovered");
+            Date date_maj = rs.getDate("date_maj");
+            String stringGlobal = ",new_confirmed: " + new_confirmed + ",total_confirmed:" + total_confirmed
+                    + ",new_deaths:" + new_deaths + ",total_deaths:" + "," + total_deaths + ",new_recovered:" + new_recovered
+                    + ",total_recovered:" + total_recovered + ",date_maj:" + date_maj;
+            System.out.println(id + "," + new_confirmed + "," + total_confirmed + "," + new_deaths + ","
+                    + total_deaths + "," + new_recovered + "," + total_recovered + "," + date_maj);
+
+            return stringGlobal;
         }
+    } catch(
+    SQLException e)
 
+    {
+        printSQLException(e);
     }
+        return "ERRORS:SELECT ERRORS GLOBAL";
+}
 
-    public void inserOrUpdatetIntoGlobal(long new_confirmed, long total_confirmed,
-                                         long new_deaths, long total_deaths,
-                                         long new_recovered, long total_recovered,
-                                         LocalDateTime date) throws SQLException {
+    public String getCountry(String countryCode) {
+
 
         // Step 1: Establishing a Connection
-        try (Connection connection = connect();
-
+        try (Connection connection = DriverManager.getConnection(url, user, password);
              // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_UPDATE_SQL)) {
-            preparedStatement.setInt(1, 1);
-            preparedStatement.setLong(2, new_confirmed);
-            preparedStatement.setLong(3, total_confirmed);
-            preparedStatement.setLong(4, new_deaths);
-            preparedStatement.setLong(5, total_deaths);
-            preparedStatement.setLong(6, new_recovered);
-            preparedStatement.setLong(7, total_recovered);
-            preparedStatement.setTimestamp(8, Timestamp.valueOf(date));
-
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY_COUNTRY);) {
+            preparedStatement.setString(1, countryCode);
+            System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            // print SQL exception information
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while(rs.next()) {
+                String country = rs.getString("country");
+                String country_code = rs.getString("country_code");
+                String slug = rs.getString("slug");
+                int new_confirmed = rs.getInt("new_confirmed");
+                int total_confirmed = rs.getInt("total_confirmed");
+                int new_deaths = rs.getInt("new_deaths");
+                int total_deaths = rs.getInt("total_deaths");
+                int new_recovered = rs.getInt("new_recovered");
+                int total_recovered = rs.getInt("total_recovered");
+                Date date_maj = rs.getDate("date_maj");
+                String stringCountry = "country:" + country + ",country_code:" + country_code + ",slug:"
+                        + slug + ",new_confirmed: " + new_confirmed + ",total_confirmed:" + total_confirmed
+                        + ",new_deaths:" + new_deaths + ",total_deaths:" + "," + total_deaths + ",new_recovered:"
+                        + new_recovered + ",total_recovered:" + total_recovered + ",date_maj:" + date_maj;
+
+                System.out.println(country + "," + country_code + "," + slug + "," + new_confirmed + ","
+                        + total_confirmed + "," + new_deaths + "," + total_deaths + ","
+                        + new_recovered + "," + total_recovered + "," + date_maj);
+
+                return stringCountry;
+            }
+        } catch(
+                SQLException e)
+
+        {
             printSQLException(e);
         }
-
+        return "ERRORS:SELECT ERRORS COUNTRY(V_PAYS)";
     }
+    public String getAvgConfirmed() {
 
-    public void inserOrUpdatetIntoCountry(String country, String countryCode, String slug,
-                                          long new_confirmed, long total_confirmed,
-                                         long new_deaths, long total_deaths,
-                                         long new_recovered, long total_recovered,
-                                         LocalDateTime date) throws SQLException {
 
         // Step 1: Establishing a Connection
-        try (Connection connection = connect();
-
+        try (Connection connection = DriverManager.getConnection(url, user, password);
              // Step 2:Create a statement using connection object
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_UPDATE_SQL_COUNTRY)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY_CONFIRMED);) {
 
-            preparedStatement.setString(1, country);
-            preparedStatement.setString(2, countryCode);
-            preparedStatement.setString(3, slug);
-            preparedStatement.setLong(4, new_confirmed);
-            preparedStatement.setLong(5, total_confirmed);
-            preparedStatement.setLong(6, new_deaths);
-            preparedStatement.setLong(7, total_deaths);
-            preparedStatement.setLong(8, new_recovered);
-            preparedStatement.setLong(9, total_recovered);
-            preparedStatement.setTimestamp(10, Timestamp.valueOf(date));
-
+            System.out.println(preparedStatement);
             // Step 3: Execute the query or update query
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            // print SQL exception information
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while(rs.next()) {
+                double avg = rs.getDouble("?column?");
+                String stringAvg = "avg:" + avg ;
+                System.out.println(stringAvg);
+                return stringAvg;
+            }
+        } catch(
+                SQLException e)
+
+        {
             printSQLException(e);
         }
+        return "ERRORS:SELECT ERRORS AVG CONFIRMED";
+    }
+    public String getAvgDeaths() {
 
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY_AVG_DEATHS);) {
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                double avg = rs.getDouble("?column?");
+                String stringAvg = "avg:" + avg;
+                System.out.println(stringAvg);
+                return stringAvg;
+            }
+        } catch (
+                SQLException e) {
+            printSQLException(e);
+        }
+        return "ERRORS:SELECT ERRORS AVG DEATHS";
     }
 
+    public String getPercentDeaths() {
+
+        // Step 1: Establishing a Connection
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_QUERY_PERCENTS_DEATHS);) {
+
+            System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: Process the ResultSet object.
+            while (rs.next()) {
+                double percent = rs.getDouble("?column?");
+                String stringPercent = "Pourcentage de mortalité:" + percent;
+                System.out.println(stringPercent);
+                return stringPercent;
+            }
+        } catch (
+                SQLException e) {
+            printSQLException(e);
+        }
+        return "ERRORS:SELECT ERRORS PERCENT";
+    }
     public static void printSQLException(SQLException ex) {
         for (Throwable e: ex) {
             if (e instanceof SQLException) {
